6ee32b702ee18aac810f0004c3beca629fda49e2
LEJEUNE Pierre <l17011117@a-maq-iutinfo.salsa.univ-amu.fr>
2018-10-10 11:00:54 +0200

    Ajout de la page erreur 404

--- Views/viewError.php
+++ Views/viewError.php
@@ -7,4 +7,6 @@
  */
 
 ?>
-<h1>ERREUR ViewError</h1>
+<body bgcolor="#EBC5D5">
+<center><h1>Erreur 404 (la fameuse), la page que vous recherchez n'existe pas</h1><br/>Revenez à l'acceuil en cliquant <a href="/">ici</a> <br/><p>Ce n'est pas le barbecue que vous recherchez</p><br/><img src="../src/img/404.gif"></center>
+</body>
